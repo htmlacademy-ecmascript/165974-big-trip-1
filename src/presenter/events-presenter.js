c935941ae645b72ba2f246bbee63d859f6bc0c05
Elena Markova <153465540+enlerna@users.noreply.github.com>
2023-12-29 01:45:44 +0300

    Добавит моковые данные, объединит форму добавления и редактирования точки маршрута

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -2,13 +2,11 @@ import {render} from '../render.js';
 import EventsListView from '../view/events-list-view.js';
 import EventsListItemView from '../view/events-list-item-view.js';
 import EventView from '../view/event-view.js';
-import EventEditView from '../view/event-edit-view.js';
-import EventAddView from '../view/event-add-view.js';
+import EventFormView from '../view/event-form-view.js';
 
 export default class EventsPresenter {
   eventsListComponent = new EventsListView();
-  eventEditComponent = new EventEditView();
-  eventAddComponent = new EventAddView();
+  eventFormComponent = new EventFormView();
 
   constructor({container}) {
     this.container = container;
@@ -17,18 +15,14 @@ export default class EventsPresenter {
   init() {
     render(this.eventsListComponent, this.container);
 
-    const eventListItemEditComponent = new EventsListItemView();
-    render(new EventEditView(), eventListItemEditComponent.getElement());
-    render(eventListItemEditComponent, this.eventsListComponent.getElement());
+    const eventListItemFormComponent = new EventsListItemView();
+    render(new EventFormView(), eventListItemFormComponent.getElement());
+    render(eventListItemFormComponent, this.eventsListComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
       const eventListItemComponent = new EventsListItemView();
       render(new EventView(), eventListItemComponent.getElement());
       render(eventListItemComponent, this.eventsListComponent.getElement());
     }
-
-    const eventListItemAddComponent = new EventsListItemView();
-    render(new EventAddView(), eventListItemAddComponent.getElement());
-    render(eventListItemAddComponent, this.eventsListComponent.getElement());
   }
 }
