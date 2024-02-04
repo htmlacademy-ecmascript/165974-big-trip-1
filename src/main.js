576b64c9d6c1fdd49a670f79010b74401740b6f3
Elena Markova <153465540+enlerna@users.noreply.github.com>
2024-02-05 01:21:05 +0300

    Создаст Filter Presenter

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import { render, RenderPosition } from './framework/render.js';
 import ButtonNewEventView from './view/button-new-event-view.js';
 import TripInfoView from './view/trip-info-view.js';
-import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
 import EventsPresenter from './presenter/events-presenter.js';
 import EventsModel from './model/events-model.js';
 import DataService from './service/data-service.js';
-import { generateFilter } from './mock/filter-data.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import { sortCriteria as criteria } from './mock/sort-data.js';
 
 const siteTripMainElement = document.querySelector('.trip-main');
-const siteTripControlsElement = document.querySelector('.trip-controls');
+const siteTripFiltersElement = document.querySelector('.trip-controls__filters');
 const siteTripEventsElement = document.querySelector('.trip-events');
 
 const dataService = new DataService();
@@ -21,13 +20,16 @@ const eventsPresenter = new EventsPresenter({
   eventsModel,
 });
 
-const filters = generateFilter(eventsModel.events);
+const filterPresenter = new FilterPresenter({
+  container: siteTripFiltersElement,
+  eventsModel,
+});
 
 render(new TripInfoView({
   events: eventsModel.events
 }), siteTripMainElement, RenderPosition.AFTERBEGIN);
 render(new ButtonNewEventView(), siteTripMainElement);
-render(new FilterView({ filters }), siteTripControlsElement);
 render(new SortView({ criteria }), siteTripEventsElement);
 
 eventsPresenter.init();
+filterPresenter.init();
